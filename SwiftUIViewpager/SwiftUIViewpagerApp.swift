b742380cd7314475ab7e473fa3602a811d7f1238
manuaravind <manuaravindpta@gmail.com>
2024-10-04 23:35:21 +0530

    Initial Commit

---
+++ SwiftUIViewpager/SwiftUIViewpagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewpagerApp.swift
+//  SwiftUIViewpager
+//
+//  Created by Manu Aravind on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewpagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
